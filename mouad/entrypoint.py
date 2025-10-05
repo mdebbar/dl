bd8e350a64b3c4f070ce42614f11b5aba6c3cb70
Mouad Debbar <mouad.debbar@gmail.com>
2025-10-05 15:31:19 -0400

    initial implementation of the one command

--- mouad/entrypoint.py
+++ mouad/entrypoint.py
@@ -1,13 +1,16 @@
 import sys
 
+CRONJOB_CONFIG = '/app/mouad/ytdlp_cronjob.conf'
+ONE_CONFIG = '/app/mouad/ytdlp_one.conf'
+
 def main(args):
     cmd = args.pop(0)
 
     match cmd:
-        case 'one':
-            return one_cmd(args)
         case 'cronjob':
             return cronjob_cmd(args)
+        case 'one':
+            return one_cmd(args)
         case _:
             print(f'Unknown command: {cmd}')
             print('Available commands: one, cronjob')
@@ -15,7 +18,19 @@ def main(args):
 
 
 def one_cmd(args):
-    print('Running command `one` with args:', args)
+    if (len(args) != 2):
+        print('The `one` command requires exactly two arguments: <id> <url>')
+        sys.exit(1)
+
+    (id, url) = args
+
+    from yt_dlp import YoutubeDL
+    with YoutubeDL(['--config-location', ONE_CONFIG]) as ydl:
+        # 1. Parse <id> to extract: <series>, <season_number>, <episode_number>
+        # 2. Pass those as extra_info to ydl.extract_info()
+        ydl.__download_wrapper(ydl.extract_info)(url, extra_info={'id': id})
+        # 3. If download is successful, append the <id> to the download archive
+        return ydl._download_retcode
 
 
 def cronjob_cmd(args):
@@ -24,7 +39,7 @@ def cronjob_cmd(args):
         sys.exit(1)
 
     import yt_dlp
-    yt_dlp.main(['--config-location', '/app/mouad/cronjob.conf'])
+    yt_dlp.main(['--config-location', CRONJOB_CONFIG])
 
 
 if __name__ == '__main__':
