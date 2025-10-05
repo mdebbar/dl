b0476658178c0446c91711629f7a5befb65550f2
Mouad Debbar <mouad.debbar@gmail.com>
2025-10-05 13:47:06 -0400

    fixes to docker_one_off.sh

--- docker_one_off.sh
+++ docker_one_off.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+
 # Name of the TV Show e.g. "Uzak Sehir"
 NAME="$1"
 
@@ -7,6 +10,9 @@ SEASON_EPISODE="$2"
 # URL of the video or m3u8 file to download.
 DOWNLOAD_URL="$3"
 
+# Download isn't useful since these downloads don't have the right ID.
+# --download-archive /app/downloads/downloaded.txt \
+
 docker run \
   --rm \
   -v /mnt/Universe/Media/Ourida:/app/downloads \
@@ -14,7 +20,6 @@ docker run \
   -N 8 \
   --proxy "socks5://10.10.10.10:1080" \
   --fixup never \
-  --download-archive /app/downloads/downloaded.txt \
   -o "/app/downloads/$NAME/$NAME $SEASON_EPISODE.%(ext)s" \
   --no-config \
-  "$3"
+  "$DOWNLOAD_URL"
