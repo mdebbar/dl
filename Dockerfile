f5f549e71c4e847e678e7a32c1c988a975dfd249
Mouad Debbar <mouad.debbar@gmail.com>
2025-10-05 16:31:07 -0400

    support one off downloads for aradrama

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 # To build it:
 # docker build -t yt-dlp-docker .
 
-# To run it:
-# docker run --rm -v /path/to/downloads:/app/downloads yt-dlp-docker -P /app/downloads
+# To run the cronjob:
+# docker run --rm -v /mnt/Universe/Media/Ourida:/app/downloads yt-dlp-docker cronjob
+
+# To download one 3isk video:
+# docker run --rm -v /mnt/Universe/Media/Ourida:/app/downloads yt-dlp-docker 3isk <id> <url>
+
+# To download one Aradrama video:
+# docker run --rm -v /mnt/Universe/Media/Ourida:/app/downloads yt-dlp-docker aradrama <id> <url>
 
 FROM python:3.11-slim
 
--- mouad/entrypoint.py
+++ mouad/entrypoint.py
@@ -4,7 +4,8 @@ import sys
 from urllib.parse import urlparse
 
 CRONJOB_CONFIG = '/app/mouad/ytdlp_cronjob.conf'
-ONE_CONFIG = '/app/mouad/ytdlp_one.conf'
+ISK_CONFIG = '/app/mouad/ytdlp_3isk.conf'
+ARADRAMA_CONFIG = '/app/mouad/ytdlp_aradrama.conf'
 
 def main(args):
     cmd = args.pop(0)
@@ -12,11 +13,15 @@ def main(args):
     match cmd:
         case 'cronjob':
             return cronjob_cmd(args)
-        case 'one':
-            return one_cmd(args)
+        case '3isk':
+            return isk_cmd(args)
+        case 'aradrama':
+            return aradrama_cmd(args)
+        case 'ara': # alias for aradrama
+            return aradrama_cmd(args)
         case _:
             print(f'Unknown command: {cmd}')
-            print('Available commands: one, cronjob')
+            print('Available commands: cronjob, 3isk')
             sys.exit(1)
 
 
@@ -30,12 +35,12 @@ def cronjob_cmd(args):
     yt_dlp.main(['--config-location', CRONJOB_CONFIG])
 
 
-def one_cmd(args):
+def isk_cmd(args):
     if (len(args) != 2):
-        print('The `one` command requires exactly two arguments: <id> <url>')
+        print('The `3isk` command requires exactly two arguments: <id> <url>')
         sys.exit(1)
 
-    (id, url) = args
+    id, url = args
 
     ID_RE = r'serie-(?P<series>[\w-]+?)-season-(?P<season>\d+)-episode-(?P<episode>\d+)'
     if not re.match(ID_RE, id):
@@ -49,7 +54,7 @@ def one_cmd(args):
         sys.exit(1)
 
     from yt_dlp import YoutubeDL, parse_options
-    _, __, ___, ydl_opts = parse_options(['--config-location', ONE_CONFIG])
+    _, __, ___, ydl_opts = parse_options(['--config-location', ISK_CONFIG])
     with YoutubeDL(ydl_opts) as ydl:
         mobj = re.match(ID_RE, id)
         series = string.capwords(mobj.group('series').replace('-', ' '))
@@ -66,6 +71,42 @@ def one_cmd(args):
                 f.write(f'iskepisode {id}\n')
         return ydl._download_retcode
 
+
+def aradrama_cmd(args):
+    if (len(args) != 2):
+        print('The `aradrama` command requires exactly two arguments: <id> <url>')
+        sys.exit(1)
+
+    id, url = args
+
+    # الحلقة
+    AL_HALAKA = '%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9'
+    ID_RE = r'(?P<series>[^/]+?)-(?:' + AL_HALAKA + r'-)?(?P<episode>\d+)'
+    if not re.match(ID_RE, id):
+        print(f'Invalid id: {id}')
+        print('Expected format: <series>-[الحلقة-]<episode_number>')
+        sys.exit(1)
+
+    parsed = urlparse(url)
+    if not parsed.scheme or not parsed.netloc:
+        print(f'Invalid url: {url}')
+        sys.exit(1)
+
+    from yt_dlp import YoutubeDL, parse_options
+    _, __, ___, ydl_opts = parse_options(['--config-location', ARADRAMA_CONFIG])
+    with YoutubeDL(ydl_opts) as ydl:
+        mobj = re.match(ID_RE, id)
+        series = mobj.group('series').replace('-', ' ')
+        episode = mobj.group('episode').zfill(2)
+        ydl.extract_info(url, extra_info={
+            'id': id,
+            'series': series,
+            'episode': episode,
+        })
+        if ydl._download_retcode == 0:
+            with open('/app/downloads/downloaded.txt', 'a') as f:
+                f.write(f'aradramaepisode {id}\n')
+        return ydl._download_retcode
 
 
 if __name__ == '__main__':
